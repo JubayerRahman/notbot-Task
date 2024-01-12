fab53fea969d1bb476215605e181c21eb4a05242
JubayerRahman <jubayerr398@gmail.com>
2024-01-13 02:53:54 +0600

    The task

---
+++ script.js
@@ -0,0 +1,14 @@
+const loginButton = document.getElementById("loginButton")
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    if(email && password){
+        window.location.href = "/success.html"
+    }
+
+
+})
